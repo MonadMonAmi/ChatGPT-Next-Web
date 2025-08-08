888e2cb297ab0c726955910db5d49f80b91938ce
AndreiS <andrewdevandroid@gmail.com>
2025-08-09 00:56:05 +0200

    add: a full list of gpt-5 models

--- app/constant.ts
+++ app/constant.ts
@@ -453,9 +453,15 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   o1: "2023-10",
   "o3-mini-2025-01-31": "2023-10",
   "o3-mini": "2023-10",
-  // temporary stub
-  "gpt-5-mini": "2099-09",
-  "gpt-5": "2099-09",
+  "gpt-5-chat-latest": "2024-05",
+  "gpt-5-chat": "2024-05",
+  "gpt-5-nano-2025-08-07": "2024-05",
+  "gpt-5-nano": "2024-05",
+  "gpt-5-mini-2025-08-07": "2024-05",
+  "gpt-5-mini": "2024-05",
+  "gpt-5-2025-08-07": "2024-10",
+  "gpt-5": "2024-10",
+
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
   "gemini-pro": "2023-12",
@@ -536,7 +542,13 @@ const openaiModels = [
   "o3-mini",
   "o3",
   "o4-mini",
+  "gpt-5-chat-latest",
+  "gpt-5-chat",
+  "gpt-5-nano-2025-08-07",
+  "gpt-5-nano",
+  "gpt-5-mini-2025-08-07",
   "gpt-5-mini",
+  "gpt-5-2025-08-07",
   "gpt-5",
 ];
 
